3706aa7d9e0ed76d5fb58e7be4b88f23b58a7b3f
ds17 <ingsed@outlook.com>
2017-03-31 02:12:08 +0800

    20170331_0211

--- mooc_ex/Creative Reptile/getAQI.py
+++ mooc_ex/Creative Reptile/getAQI.py
@@ -1,9 +1,10 @@
 #D:\Python\Python35\python
 # -*- coding:utf-8 -*-
 
-import requests,os,time,re,csv
+import requests,re,csv,traceback,os,time
 from bs4 import BeautifulSoup
 
+start_time=time.strftime("%Y%m%d_%H%M%S", time.localtime())
 def robots_test(url):
     robots_url=url + 'robots.text'
     r=requests.get(robots_url)
@@ -21,13 +22,77 @@ def get_HTMLText(url):
     except:
         print("获取页面异常")
 
+def get_cityDict(cityDict,html):
+    soup = BeautifulSoup(html, 'html.parser')
+    china_aTag = soup.find('a', attrs={'href': r'http://aqicn.org/map/china/cn/'})
+    china = china_aTag.parent
+    cities = china.find_all('a')
+    for i in range(1,len(cities)):
+        name=cities[i].text
+        url=cities[i].attrs['href']
+        cityDict[name]=url
+
+def getStationAQI(city_name, city_url):
+    aqi=[]
+    html=get_HTMLText(city_url)
+    soup=BeautifulSoup(html,'html.parser')
+    try:
+        target=soup.find('div',attrs={'class':'map-stations max-height max-height-300'})
+        get_time=time.strftime("%Y%m%d_%H%M%S", time.localtime())
+        aqi.append([city_name+'(爬取时间：'+str(get_time)+')'])
+        stationTags=target.find_all('a')
+        for i in range(len(stationTags)):
+            content=stationTags[i].text
+            re_split=re.search(r'(.*?)  \((\d+)\)',content)
+            station_name=re_split.group(1)
+            station_aqi=re_split.group(2)
+            aqi.append(['',station_name,station_aqi])
+        return aqi
+    except:
+        # traceback.print_exc()
+        pass
+
+def write2csv(list):
+    csv_header=['省份/城市','站点名称','空气质量指数']
+    path=os.path.join(os.getcwd(),str(start_time) + '.csv')
+
+    not_exsits=False
+    if not os.path.exists(path):        not_exsits=True  #判断文件是否存才并保存状态
+
+    csv_file=open(path,'a',newline='')
+    writer=csv.writer(csv_file)
+
+    if not_exsits:
+        writer.writerow(csv_header)
+
+    try:
+        for item in list:
+            writer.writerow(item)
+    except:
+        # traceback.print_exc()
+        pass
+
+    csv_file.close()
+
 def main():
     base_url='http://aqicn.org/'
     if robots_test(base_url):
         start_url='http://aqicn.org/map/cn/'
-        print(get_HTMLText(start_url))
+        html=get_HTMLText(start_url)
+        cityDict={}
+        get_cityDict(cityDict,html)
+
+        for city_name, city_url in cityDict.items():
+            aqilist=getStationAQI(city_name, city_url)
+            write2csv(aqilist)
+
     else:
         print('Robots协议不可')
 
 main()
+# testDict={'成都': 'http://aqicn.org/map/chengdu/cn/', }
+# for city_name, city_url in testDict.items():
+#     aqilist=getStationAQI(city_name, city_url)
+#     write2csv(aqilist)
+
 
--- mooc_ex/test.py
+++ mooc_ex/test.py
@@ -4,21 +4,8 @@
 import csv,requests
 from bs4 import BeautifulSoup
 f_path=r'C:\users\acer\desktop\test.csv'
+import re
 
-url='https://gupiao.baidu.com/stock/sh600050.html'
-r=requests.get(url)
-r.encoding=r.apparent_encoding
-print(r.status_code)
-html=r.text
-soup=BeautifulSoup(html,'html.parser')
-stockInfo=soup.find('div',attrs={'class':'stock-bets'})
-infoDict={}
-name=stockInfo.find_all(attrs={'class':'bets-name'})[0]
-infoDict.update({'股票名称':name.text.split()[0]})
-fieldnames=[]
-keylist=stockInfo.find_all('dt')
-valuelist=stockInfo.find_all('dd')
-for i in range(len(keylist)):
-    key=keylist[i].text
-    fieldnames.append(key)
-print(fieldnames)
+str='成都  (72)'
+s=re.search(r'(.*?)  \((\d+)\)',str)
+print(s.groups())
