0de039b049756a644863197bc320043a7c0402b3
ds17 <ingsed@outlook.com>
2017-04-09 21:54:03 +0800

    20170409_2153

--- mooc_ex/test.py
+++ mooc_ex/test.py
@@ -1,11 +1,8 @@
 #D:\Python\Python35\python.exe
 # -*- coding:utf-8 -*-
 
-import csv,requests
-from bs4 import BeautifulSoup
-f_path=r'C:\users\acer\desktop\test.csv'
-import re
+str='http://python123.io/ws/demo.html'
+name=str.split('/')
+print(name)
 
-str='成都  (72)'
-s=re.search(r'(.*?)  \((\d+)\)',str)
-print(s.groups())
+
