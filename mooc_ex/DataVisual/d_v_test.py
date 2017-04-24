c0ce231af6adf97f2b8a17eca217fc6730f79785
ds17 <ingsed@outlook.com>
2017-04-24 23:10:01 +0800

    20170424_2309

--- mooc_ex/DataVisual/d_v_test.py
+++ mooc_ex/DataVisual/d_v_test.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
 import numpy as np
+from PIL import Image
+
 # a=np.arange(100).reshape((5,10,2))
 # np.savetxt('a.csv',a,fmt='%.3f',delimiter=',')
 
@@ -10,5 +12,7 @@ import numpy as np
 
 # c=np.fromfile('aa.dat',dtype=np.float,sep=',')
 # np.save('a.npy',a)
-na=np.load('a.npy')
-print(na.shape)
+im=np.array(Image.open(r"F:\pyc\PyCharm\GitHub\reptiles_gh\mooc_ex\DataVisual\sea.jpg"))
+b=[255,255,255]-im
+bw_im=  Image.fromarray(b.astype(('uint8')))
+bw_im.save(r'F:\pyc\PyCharm\GitHub\reptiles_gh\mooc_ex\DataVisual\bw_sea.jpg')
