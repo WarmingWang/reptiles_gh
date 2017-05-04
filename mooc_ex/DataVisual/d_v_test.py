3bbfb6c0982e322ee5ca37e11566fbf906963bdb
ds17 <ingsed@outlook.com>
2017-05-04 23:40:29 +0800

    20170504_2340

--- mooc_ex/DataVisual/d_v_test.py
+++ mooc_ex/DataVisual/d_v_test.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib
 
 matplotlib.rcParams['font.family']='SimHei'
-matplotlib.rcParams['font.size']=16
+matplotlib.rcParams['font.size']=10
 
 
 def f(t):
@@ -16,7 +16,6 @@ def test1():
     plt.subplot(211)
     plt.plot(a,f(a))
 
-    plt.subplot(212)
     plt.plot(a,np.cos(2*np.pi*a),'r--')
     plt.show()
 
@@ -44,6 +43,7 @@ def test4(): # subplot2grid
     a = np.arange(0.0, 5.0, 0.02)
     plt.plot(a, np.cos(2 * np.pi * a), 'r--')
     plt.show()
+
 def test5(): #gridspec
     import matplotlib.gridspec as gridspec
     gs=gridspec.GridSpec(3,3)
@@ -55,4 +55,69 @@ def test5(): #gridspec
     plt.plot(a, np.cos(2 * np.pi * a), 'r--')
     plt.show()
 
-test5()
+def plot_pie():
+    labels='Frogs','Hogs','Dogs','Logs'
+    sizes=[15,30,45,10]
+    explode=[0,0.1,0,0]
+
+    plt.pie(sizes,explode=explode,labels=labels,autopct='%1.1f%%',shadow=False,startangle=90)
+    plt.axis('equal')
+
+    plt.show()
+
+def plot_hist():
+    np.random.seed(2)
+    mu,sigma=100,20
+    a=np.random.normal(mu,sigma,size=100)
+
+    plt.hist(a,20,normed=0,histtype='barstacked',facecolor='b',alpha=0.75)
+    plt.title('直方图')
+
+    plt.show()
+
+def plot_polar():
+    np.random.seed(0)
+    n=20
+    theta=np.linspace(0.0,2*np.pi,n,endpoint=False)
+    radii=10*np.random.rand(n)
+    width=np.pi/4*np.random.rand(n)
+
+    import matplotlib.gridspec as gridspec
+    gs=gridspec.GridSpec(4,4)
+    ax=plt.subplot(gs[:3,:3],projection='polar')
+    bars=ax.bar(theta,radii,width=width,bottom=0.0)
+
+
+    for r,bar in  zip(radii,bars):
+        bar.set_facecolor(plt.cm.viridis(r/10.))
+        bar.set_alpha(0.5)
+
+
+    plt.show()
+
+
+def multi_figure():
+    # np.random.seed(0)
+    # x=np.random.rand(0,2*np.pi,100)
+    # fig1=plt.figure()
+    # fig2=plt.figure()
+    #
+    plt.figure(1)  # the first figure
+    plt.subplot(211)  # the first subplot in the first figure
+    plt.plot([1, 2, 3])
+    plt.subplot(212)  # the second subplot in the first figure
+    plt.plot([4, 5, 6])
+
+    plt.figure(2)  # a second figure
+    plt.plot([4, 5, 6])  # creates a subplot(111) by default
+
+    plt.figure(1)  # figure 1 current; subplot(212) still current
+    plt.subplot(211)  # make subplot(211) in figure1 current
+    plt.title('Easy as 1, 2, 3')  # subplot 211 title
+
+    plt.show()
+
+
+
+
+multi_figure()
