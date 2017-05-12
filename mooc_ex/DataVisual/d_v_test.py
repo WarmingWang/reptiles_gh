19bd26ff6165e321763373f3d00dee4e0058c9ae
ds17 <ingsed@outlook.com>
2017-05-12 23:37:19 +0800

    20170512_2337

--- mooc_ex/DataVisual/d_v_test.py
+++ mooc_ex/DataVisual/d_v_test.py
@@ -8,79 +8,29 @@ import pandas as pd
 matplotlib.rcParams['font.family']='SimHei'
 matplotlib.rcParams['font.size']=10
 
-
-def f(t):
-    return np.exp(-t)*np.cos(2*np.pi*t)
-
-def plot_pie():
-    labels='Frogs','Hogs','Dogs','Logs'
-    sizes=[15,30,45,10]
-    explode=[0,0.1,0,0]
-
-    plt.pie(sizes,explode=explode,labels=labels,autopct='%1.1f%%',shadow=False,startangle=90)
-    plt.axis('equal')
-
-    plt.show()
-
-def plot_hist():
-    np.random.seed(2)
-    mu,sigma=100,20
-    a=np.random.normal(mu,sigma,size=100)
-
-    plt.hist(a,20,normed=0,histtype='barstacked',facecolor='b',alpha=0.75)
-    plt.title('直方图')
-
-    plt.show()
-
-def plot_polar():
-    np.random.seed(0)
-    n=20
-    theta=np.linspace(0.0,2*np.pi,n,endpoint=False)
-    radii=10*np.random.rand(n)
-    width=np.pi/4*np.random.rand(n)
-
-    import matplotlib.gridspec as gridspec
-    gs=gridspec.GridSpec(4,4)
-    ax=plt.subplot(gs[:3,:3],projection='polar')
-    bars=ax.bar(theta,radii,width=width,bottom=0.0)
-
-
-    for r,bar in  zip(radii,bars):
-        bar.set_facecolor(plt.cm.viridis(r/10.))
-        bar.set_alpha(0.5)
-
-
-    plt.show()
-
-def multi_figure():
-    # np.random.seed(0)
-    # x=np.random.rand(0,2*np.pi,100)
-    # fig1=plt.figure()
-    # fig2=plt.figure()
-    #
-    plt.figure(1)  # the first figure
-    plt.subplot(211)  # the first subplot in the first figure
-    plt.plot([1, 2, 3])
-    plt.subplot(212)  # the second subplot in the first figure
-    plt.plot([4, 5, 6])
-
-    plt.figure(2)  # a second figure
-    plt.plot([4, 5, 6])  # creates a subplot(111) by default
-
-    plt.figure(1)  # figure 1 current; subplot(212) still current
-    plt.subplot(211)  # make subplot(211) in figure1 current
-    plt.title('Easy as 1, 2, 3')  # subplot 211 title
-
-    plt.show()
-
-def shape_test():
-    a=np.arange(20).reshape(4,5)
-    print(a)
-    print(a.shape[0])
-
 def pd_test():
-    d=pd.Series({'a':9,'b':8,'c':7})
+    # d=pd.Series({'a':9,'b':8,'c':11})
+    # d.name='Series test object'
+    # d.index.name='Series index test name'
+    # user=d.index.name
+    # print(d,user)
+    dt={
+        'one':pd.Series([1,2,3],index=['a','b','c']),
+        'two':pd.Series([9,8,7,6],['a','b','c','d'])
+    }
+    dl={
+        '城市':['北京','上海','广州','深圳','沈阳'],
+        '环比':[101.5,101.2,101.3,102.0,100.1],
+        '同比':[120.7,127.3,119.4,140.9,101.4],
+        '定基':[121.4,127.8,120.8,145.5,101.6]
+    }
+
+    d=pd.DataFrame(dl,index=['r1','r2','r3','r4','r5'])
+
     print(d)
+
+
+
 
 
 pd_test()
