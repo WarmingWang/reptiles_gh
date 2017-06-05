bb5431784017f89336bb0fd1e02b49893ec25639
ds17 <ingsed@outlook.com>
2017-06-05 21:54:47 +0800

    20170605_2154

--- Machine Learning/test1.py
+++ Machine Learning/test1.py
@@ -1,1 +1,27 @@
 # -*- coding:utf-8 -*-
+
+import matplotlib.pyplot as plt
+from sklearn import datasets
+import seaborn as sns
+# import csv
+
+from sklearn import decomposition
+from sklearn import manifold
+from matplotlib.ticker import NullFormatter
+from time import time
+
+
+def write2csv(item):
+    pass
+
+n_componets=2
+n_neighbors=10
+
+data=datasets.load_iris()
+x=data.data
+color=data.target
+
+fig=plt.figure(figsize=(15,4))
+
+t0=time()
+
