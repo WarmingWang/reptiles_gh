f62e7b9bbf3b4601eeb3d0e090c49314f80c1824
ds17 <ingsed@outlook.com>
2017-06-11 13:50:08 +0800

    add data ch04

--- Machine Learning/python for DA/ch02 ex.py
+++ Machine Learning/python for DA/ch02 ex.py
@@ -4,3 +4,16 @@ import json
 path='data/ch02/usagov_bitly_data2012-03-16-1331923249.txt'
 
 records=[json.loads(line) for line in open(path)]
+time_zones=[rec['tz'] for rec in records if 'tz' in rec]
+
+def get_counts(sequense):
+    counts={}
+    for x in sequense:
+        if x in counts:
+            counts[x]+=1
+        else:
+            counts[x]=1
+    return counts
+
+
+print(get_counts(time_zones))
