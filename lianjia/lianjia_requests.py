d23f2d1e295950ca448e5106047c982792c808c7
ds17 <ingsed@outlook.com>
2017-09-25 22:05:08 +0800

    add func

--- lianjia/lianjia_requests.py
+++ lianjia/lianjia_requests.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 
 import requests
+from bs4 import BeautifulSoup
 
 url_base='http://sh.lianjia.com/ershoufang/d'
 
@@ -13,10 +14,24 @@ def get_html(url):
     except:
         print("索引页面获取异常")
 
-def parse_index_page(index_page):
-    pass
+def parse_index_page(html):
+    fieldnames=['title','type','area','']
+    soup=BeautifulSoup(html,'html.parser')
+    index_info=soup.find_all('a',attrs={'class':'text link-hover-green js_triggerGray js_fanglist_title'})
+    return index_info
+
+
 
 def parse_detail_page(detail_page):
     pass
 
+
+def main():
+    url=url_base + "1"
+    index_page=get_html(url)
+    info=parse_index_page(index_page)
+
+
+
+main()
 
