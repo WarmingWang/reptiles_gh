55ab1c13aff0443700bc77d16e305d34bff82d2c
ds17 <ingsed@outlook.com>
2017-11-09 01:35:30 +0800

    update lianjia trends py file

--- lianjia/lianjia_trend.py
+++ lianjia/lianjia_trend.py
@@ -1,9 +1,6 @@
 
 # coding: utf-8
 
-# In[11]:
-
-
 import requests,time,os,csv,re,logging
 from bs4 import BeautifulSoup
 import numpy as np
@@ -12,28 +9,13 @@ import traceback
 from lianjia_requests import get_n_times
 
 
-# In[37]:
-
-
 ex_date = time.strftime('%Y%m%d')
 ex_time = time.strftime('%H:%M:%S')
 
-
-# In[ ]:
-
-
-print(ex_date + ex_time+ "：正在获取...")
-
-
-# In[38]:
-
+print(ex_date + "_" + ex_time+ " 正在获取..." + "\n")
 
 url = 'http://sh.lianjia.com/ershoufang'
 html =get_n_times(url, n=6)
-
-
-# In[42]:
-
 
 soup=BeautifulSoup(html,'html.parser')
 spans=soup.find_all('span',attrs={'class':'num strong-num'})
@@ -42,19 +24,11 @@ total_on_sale=spans[0].string
 last_90day_deal = spans[1].string
 yesterday_viewed = spans[1].string
 
-
-# In[45]:
-
-
 item={'ex_date':ex_date,
       'ex_time':ex_time, 
       'total_on_sale':total_on_sale,
       'last_90day_deal':last_90day_deal,
       'yesterday_viewed':yesterday_viewed}
-
-
-# In[51]:
-
 
 path=os.path.join(os.getcwd(),'results/lianjia_trends.csv')
 
@@ -70,9 +44,6 @@ csv_file=open(path,'a+',newline='')
 writer=csv.DictWriter(csv_file,fieldnames=fieldnames)
 writer.writerow(item)
 csv_file.close()
-
-
-# In[56]:
 
 
 n=3
